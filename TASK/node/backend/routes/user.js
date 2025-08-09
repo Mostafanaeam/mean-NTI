ea61f7cc0cce586e69c38636051cc7f675fe1719
Mostafa Naeam <mnaeam10@gmail.com>
2025-08-09 14:41:29 +0300

    delet task folder

--- TASK/node/backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const route=express.Router()
-
-const{getuser,postuser,login}=require('../controller/user')
-route.get('/',getuser)
-route.post('/',postuser)
-route.post('/login',login)
-module.exports=route
